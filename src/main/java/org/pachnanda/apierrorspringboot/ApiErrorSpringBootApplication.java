6e2e383f81017ed6fa2f6b958ff2778ec6220725
Gautam Pachnanda <gautampachnanda@gautams-mbp-2.home>
2019-12-08 21:57:41 +0000

    Initial commit spring boot

---
+++ src/main/java/org/pachnanda/apierrorspringboot/ApiErrorSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.pachnanda.apierrorspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiErrorSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiErrorSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pachnanda/apierrorspringboot/ApiErrorSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pachnanda.apierrorspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiErrorSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
